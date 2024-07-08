c2613fcd0b7ee32ea83355293a7babb78c101712
zheqi <2481550454@qq.com>
2024-07-09 00:48:15 +0800

    feat: 移除password.js

--- server/test/password.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const path = require('node:path');
-require('dotenv').config({ path: path.join(__dirname, '../src/.env') });
-const HmacSHA256 = require('crypto-js/hmac-sha256');
-
-const key = process.env.psdSecret;
-console.log(HmacSHA256('carole', key).toString());
-console.log(HmacSHA256('123456', key).toString());

490506c26bf688f7f8e51101690853f65444e5ef
zheqi <2481550454@qq.com>
2024-07-13 00:42:16 +0800

    feat: 更新定时任务

--- server/src/module/monitor/job/job.service.ts
+++ server/src/module/monitor/job/job.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { isNotEmpty } from 'class-validator';
 import { SchedulerRegistry } from '@nestjs/schedule';
@@ -10,10 +10,30 @@ import { exportTable } from '@/common/utils/export';
 import { buildQueryCondition } from '@/common/utils';
 
 @Injectable()
-export class JobService {
-  private readonly logger = new Logger(JobService.name);
-
+export class JobService implements OnModuleInit {
   constructor(private prisma: PrismaService, private scheduler: SchedulerRegistry) {}
+  onModuleInit() {
+    this.startAllJob();
+  }
+
+  // 查找所有状态为1的任务，并启动
+  async startAllJob() {
+    const jobs = await this.prisma.sysJob.findMany({ where: { status: '0' } });
+
+    jobs.forEach((job) => {
+      try {
+        const { invokeTarget, cronExpression } = job;
+
+        const cronJob = new CronJob(cronExpression, this[invokeTarget]);
+
+        this.scheduler.addCronJob(invokeTarget, cronJob);
+
+        cronJob.start();
+      } catch (err) {
+        console.error(`任务创建失败, ${err}`);
+      }
+    });
+  }
 
   async selectJobList(q: QueryJobDto) {
     const conditions = {
@@ -45,22 +65,103 @@ export class JobService {
       !isNotEmpty(job[key]) && delete job[key];
     }
 
-    return this.prisma.sysJob.create({ data: job });
-  }
+    if (job.misfirePolicy === '2') {
+      try {
+        this[job.invokeTarget]();
+      } catch (error) {
+        return new BadRequestException(`任务执行失败, ${error}`);
+      }
 
-  async updateJob(sysJob: UpdateSysJobDto) {
-    for (const key in sysJob) {
-      !isNotEmpty(sysJob[key]) && delete sysJob[key];
+      job.status = '1';
+
+      const res = await this.prisma.sysJob.create({ data: job });
+
+      return res;
     }
 
-    return this.prisma.sysJob.update({
-      where: { jobId: sysJob.jobId },
-      data: sysJob,
+    const res = await this.prisma.sysJob.create({ data: job });
+
+    const { invokeTarget, status, cronExpression, misfirePolicy } = res;
+
+    if (misfirePolicy === '3' || status === '1') { return res; }
+
+    try {
+      const cronJob = new CronJob(cronExpression, this[invokeTarget]);
+
+      this.scheduler.addCronJob(invokeTarget, cronJob);
+
+      cronJob.start();
+    } catch (err) {
+      return new BadRequestException(`任务创建失败, ${err}`);
+    }
+
+    return res;
+  }
+
+  async updateJob(job: UpdateSysJobDto) {
+    for (const key in job) {
+      !isNotEmpty(job[key]) && delete job[key];
+    }
+
+    if (job.misfirePolicy === '2') {
+      try {
+        this[job.invokeTarget]();
+      } catch (error) {
+        return new BadRequestException(`任务执行失败, ${error}`);
+      }
+
+      job.status = '1';
+
+      const res = await this.prisma.sysJob.update({
+        where: { jobId: job.jobId },
+        data: job,
+      });
+
+      return res;
+    }
+
+    const res = await this.prisma.sysJob.update({
+      where: { jobId: job.jobId },
+      data: job,
     });
+
+    const { invokeTarget, status, cronExpression, misfirePolicy } = res;
+
+    const scheduledJob = this.scheduler.getCronJob(invokeTarget);
+
+    if (scheduledJob) {
+      scheduledJob.stop();
+    }
+
+    this.scheduler.deleteCronJob(invokeTarget);
+
+    if (misfirePolicy === '3' || status === '1') { return res; }
+
+    const cronJob = new CronJob(cronExpression, this[invokeTarget]);
+
+    this.scheduler.addCronJob(invokeTarget, cronJob);
+
+    cronJob.start();
+
+    return res;
   }
 
   async deleteJob(jobId: number) {
-    return this.prisma.sysJob.delete({ where: { jobId } });
+    const res = await this.prisma.sysJob.delete({ where: { jobId } });
+
+    try {
+      const scheduledJob = this.scheduler.getCronJob(res.invokeTarget);
+
+      if (scheduledJob) {
+        scheduledJob.stop();
+      }
+
+      this.scheduler.deleteCronJob(res.invokeTarget);
+    } catch {
+      return new BadRequestException('任务已删除，但是定时任务删除时报错');
+    }
+
+    return res;
   }
 
   async exportJob(res: Response) {
@@ -85,56 +186,27 @@ export class JobService {
     exportTable(data, res);
   }
 
-  async changeStatus(sysJob: ChangeSysJobStatusDto) {
-    return this.prisma.sysJob.update({
-      where: { jobId: sysJob.jobId },
-      data: { status: sysJob.status },
-    });
-  }
-
-  /**
-   * Get all cron jobs
-   */
-  getCrons() {
-    const jobs = this.scheduler.getCronJobs();
-    jobs.forEach((value, key, _map) => {
-      let next: string | Date;
-      try {
-        const nextDates = value.nextDates();
-        if (nextDates.length > 0) {
-          next = nextDates[0].toJSDate();
-        } else {
-          next = 'error: no next dates available';
-        }
-      } catch {
-        next = 'error: next fire date is in the past!';
-      }
-      this.logger.log(`job: ${key} -> next: ${next}`);
-    });
-  }
-
-  /**
-   * Add a new cron job
-   * @param name name of the job
-   * @param cronTime cron time
-   */
-  addCronJob(name: string, cronTime: string) {
-    const job = new CronJob(cronTime, () => {
-      this.logger.warn(`time (${cronTime}) for job ${name} to run!`);
+  async changeStatus(job: ChangeSysJobStatusDto) {
+    const res = await this.prisma.sysJob.update({
+      where: { jobId: job.jobId },
+      data: { status: job.status },
     });
 
-    this.scheduler.addCronJob(name, job);
-    job.start();
+    const { invokeTarget, status } = res;
 
-    this.logger.warn(`job ${name} added for each minute at ${cronTime} seconds!`);
+    const scheduledJob = this.scheduler.getCronJob(invokeTarget);
+
+    if (status === '1') {
+      scheduledJob.stop();
+    } else {
+      scheduledJob.start();
+    }
+
+    return res;
   }
 
-  /**
-   * Delete a cron job
-   * @param name name of the job
-   */
-  deleteCron(name: string) {
-    this.scheduler.deleteCronJob(name);
-    this.logger.warn(`job ${name} deleted!`);
+  async testJob() {
+    // eslint-disable-next-line no-console
+    console.log('testJob');
   }
 }
